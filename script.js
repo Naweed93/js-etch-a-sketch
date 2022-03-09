45f55400769257ac045b9117b234fc1dbd83f9ff
navid ashraf <ashraf.navid71@gmail.com>
2022-03-10 00:53:42 +0330

    Let user change grid size user can change row and column size. also it is possible to clear the grid.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const container = document.createElement('div');
 const body = document.querySelector('body');
-
+const clear = document.querySelector('.clear');
+const set = document.querySelector('.set');
+const rowInput = document.querySelector('.row-input');
+const columnInput = document.querySelector('.column-input');
 
 body.appendChild(container);
 container.classList.add('container');
@@ -12,8 +15,23 @@ function createGrid(rowNumber, columnNumber){
         for(let j = 0; j<columnNumber; j++){
             let column = document.createElement('div');
             column.classList.add('column',`column-${j}`);
-            row.appendChild(column)
+            row.appendChild(column);
+            if (rowNumber !==16){
+                
+                column.style.width=960/rowNumber + 'px';
+                column.style.height=960/rowNumber + 'px';
+            }
         }
     }
 }
 createGrid(16,16);
+
+clear.addEventListener('click',(e) => {
+    container.innerHTML=""
+});
+set.addEventListener('click', (e) =>{
+    if ( Number(rowInput.value) && Number(columnInput.value) ){
+        container.innerHTML=""
+        createGrid(Number(columnInput.value), Number(rowInput.value))
+    }
+});
